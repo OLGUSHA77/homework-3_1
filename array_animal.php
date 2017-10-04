ee1cc1f4860c1ddea3c41a8c7ab73ee86b9d5384
OLGA <olgusha0707@mail.ru>
2017-10-04 20:45:26 +0300

    commit 1

--- array_animal.php
+++ array_animal.php
@@ -5,11 +5,12 @@ $mass = [
     "Arctica" => array("Vulpes lagopus","Odobenus rosmarus","Ursus maritimus","Orcinus orca"),
     "Africa" => array("Equus","Panthera pardus","Crocodylus niloticus","Giraffa camelopardalis"),
 ];
-$newMass = [];
+$mass1 = [];
+$mass2 = [];
 $space = " ";
 foreach ($mass as $key => $value) {
     foreach($value as $animal ) {
-        if (substr_count($animal, $space) == 1) {
+        if (str_word_count($animal) == 2) {
             $str = explode($space, $animal);
             $mass1[] = $str[0];
             $mass2[] = $str[1];
@@ -19,11 +20,12 @@ foreach ($mass as $key => $value) {
 
 shuffle($mass1);
 shuffle($mass2);
-$allMass = array();
-foreach ($mass1 as $key => $value) {
-    $allMass[] = $value . ' ' . $mass2[$key];
-}
 
+$allMass = [];
+foreach ($mass1 as $key => $value) {
+    $allMass[] = $value . $space . $mass2[$key];
+}
+var_dump($allMass);
 echo '<h2>Животные по континентам</h2>';
 foreach ($mass as $key => $value) {
 
@@ -32,10 +34,10 @@ foreach ($mass as $key => $value) {
 
     foreach ($value as $animal) {
 
-        $m1 = explode(" ", $animal);
+        $m1 = explode($space, $animal);
 
         foreach ($allMass as $fantasticAnimal) {
-            $m2 = explode(" ", $fantasticAnimal);
+            $m2 = explode($space, $fantasticAnimal);
 
             if ($m1[0] == $m2[0]) {
                 $printMass[] = $fantasticAnimal;
